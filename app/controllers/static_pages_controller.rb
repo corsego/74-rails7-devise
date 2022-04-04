f5d5656cb5b699ed55a6d8ba72fab67fcf10e23d
Yaro <yshmarov@gmail.com>
2022-04-04 20:26:28 +0000

    make all devise forms and links work

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 class StaticPagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[ landing_page ]
+
   def landing_page
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'static_pages/landing_page'
-  get 'static_pages/dashboard'
+  get 'dashboard', to: 'static_pages#dashboard'
   root "static_pages#landing_page"
 end
